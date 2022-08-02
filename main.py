8c1f5fe0f12a5379d1270d1d43861a6b181664da
Hudson Reamer <hreamer@purdue.edu>
2022-08-02 02:54:47 -0400

    Made main execution loop

---
+++ main.py
@@ -0,0 +1,12 @@
+
+#Main Loop
+from operator import eq
+from tkinter import E
+
+
+while True:
+    #Get Input from the User 
+    equation = input("Input: ")
+    #print("The user inputed " + equation)
+
+    #Now we have to parse the input into tokens
